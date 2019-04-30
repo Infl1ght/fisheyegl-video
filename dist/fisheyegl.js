b07b9b295b536b1ac1482dded2dfbebb4a1f4b05
ilya.volkov <ilya.inflight@gmail.com>
2019-04-30 16:39:43 +0300

    Unused functions removed

--- dist/fisheyegl.js
+++ dist/fisheyegl.js
@@ -85,7 +85,6 @@ const FisheyeGl = function FisheyeGl(opts) {
     x: 1.0,
     y: 1.0,
   };
-  const image = options.image || 'images/barrel-distortion.png';
 
   const gl = getGLContext(options.canvas);
 
@@ -122,46 +121,6 @@ const FisheyeGl = function FisheyeGl(opts) {
   }
 
   createBuffers();
-
-  function loadImage(gl, img, callback, texture) {
-    texture = texture || gl.createTexture();
-
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR); // gl.NEAREST is also allowed, instead of gl.LINEAR, as neither mipmap.
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); // Prevents s-coordinate wrapping (repeating).
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE); // Prevents t-coordinate wrapping (repeating).
-    // gl.generateMipmap(gl.TEXTURE_2D);
-    gl.bindTexture(gl.TEXTURE_2D, null);
-
-    if (callback) callback(null, texture);
-    return texture;
-  }
-
-  function resize(w, h) {
-    gl.viewport(0, 0, w, h);
-    gl.canvas.width = w;
-    gl.canvas.height = h;
-  }
-
-  function loadImageFromUrl(gl, url, callback) {
-    const texture = gl.createTexture();
-    const img = new Image();
-    img.addEventListener('load', () => {
-      loadImage(gl, img, callback, texture);
-      options.width = img.width;
-      options.height = img.height;
-      resize(
-        options.width,
-        options.height,
-      );
-    });
-    img.src = url;
-    return texture;
-  }
 
   function run(animate, callback) {
     const f = window.requestAnimationFrame || window.mozRequestAnimationFrame
@@ -217,16 +176,9 @@ const FisheyeGl = function FisheyeGl(opts) {
     gl.drawElements(gl.TRIANGLES, model.indices.length, gl.UNSIGNED_SHORT, 0);
   };
 
-  function setImage(imageUrl, callback) {
-    texture = loadImageFromUrl(gl, imageUrl, () => {
-      run(options.animate, callback);
-    });
-  }
-
   function setVideo(video) {
     texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
-  
     // Because video has to be download over the internet
     // they might take a moment until it's ready so
     // put a single pixel in the texture so we can
@@ -259,31 +211,13 @@ const FisheyeGl = function FisheyeGl(opts) {
       run();
     }, 100);
   }
-  // setImage(image);
 
-  // asynchronous!
-  function getImage(format) {
-    const img = new Image();
-
-    img.src = gl.canvas.toDataURL(format || 'image/jpeg');
-
-    return img;
-  }
-
-  function getSrc(format) {
-    return gl.canvas.toDataURL(format || 'image/jpeg');
-  }
-
-  // external API:
   const distorter = {
     options,
     gl,
     lens,
     fov,
     run,
-    getImage,
-    setImage,
-    getSrc,
     setVideo,
   };
 
