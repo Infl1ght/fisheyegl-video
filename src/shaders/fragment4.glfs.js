de0cd0391d29a34f178d44cc1bebbe423e580a31
ilya.volkov <ilya.inflight@gmail.com>
2019-08-14 10:46:49 +0300

    Small perfomance improvement

--- src/shaders/fragment4.glfs.js
+++ src/shaders/fragment4.glfs.js
@@ -12,7 +12,7 @@ uniform sampler2D uSampler;\n\
 varying vec3 vPosition;\n\
 varying vec2 vTextureCoord;\n\
 vec2 GLCoord2TextureCoord(vec2 glCoord) {\n\
-	return glCoord * vec2(1.0, -1.0) / 2.0 + vec2(0.5, 0.5);\n\
+	return glCoord * vec2(1.0, -1.0) + vec2(0.5, 0.5);\n\
 }\n\
 void main(void){\n\
 	float scale = uScale;\n\
@@ -38,8 +38,8 @@ void main(void){\n\
   float r6 = r2 * r2 * r2;\n\
   float rescale = pow(2.0, - scale / 100.0);\n\
   float radius_mult = rescale * (k3 * r2 + k5 * r4 + k7 * r6 + 1.0);\n\
-	vMapping.x = (centerX + radius_mult * off_x) / uSize[0] * 2.0;\n\
-	vMapping.y = (centerY + radius_mult * off_y) / uSize[1] * 2.0;\n\
+	vMapping.x = (centerX + radius_mult * off_x) / uSize[0];\n\
+	vMapping.y = (centerY + radius_mult * off_y) / uSize[1];\n\
 	vMapping = GLCoord2TextureCoord(vMapping);\n\
 	vec4 texture = texture2D(uSampler, vMapping);\n\
 	if(vMapping.x > 0.9999 || vMapping.x < 0.0001 || vMapping.y > 0.9999 || vMapping.y < 0.0001){\n\
